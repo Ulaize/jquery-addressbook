6a569c072f055283ac5f38905e8aae5d77d08488
Catherine <ducharme.catherine@gmail.com>
2015-10-14 17:49:50 +0000

    Got started with Backbone.Router in app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 var displayFunctions = require('./lib/display');
+var Backbone = require('backbone');
 // Add foundation dynamic functionality on page
 $(document).foundation();
 
+var router = Backbone.Router.extend({
+    routes: {
+        '': 'displayList',
+        'addressbooks(/page:pageNum)': 'showAddressBooks',
+        'addressbooks/:id(/page:pageNum)': 'showAddressBook',
+        'entry/:id': 'showEntry'
+    },
+    displayList: displayFunctions.displayAddressBooksList(0, 5),
+    showAddressBooks: displayFunctions.displayAddressBooksList,
+    showAddressBook: displayFunctions.displayAddressBook,
+    showEntry: displayFunctions.displayEntry
+});
+
+var thisRouter = new router;
+Backbone.history.start();
+
 // Start the app by displaying all the addressbooks
-displayFunctions.displayAddressBooksList(0, 3);
-
-
+// 
